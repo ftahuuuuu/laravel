432cd0548481b7c0070fc3f402b4ff01bf69483f
ftahuuuuu <fattahuanma@gmail.com>
2021-02-24 12:39:08 +0700

    auth laravel
    auth laravel

--- blog/resources/views/master/_navigasi.blade.php
+++ blog/resources/views/master/_navigasi.blade.php
@@ -10,15 +10,17 @@
           <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
         </li>
         <li class="nav-item">
+          <a class="nav-link" href="/login">Login</a>
+        </li>
+        <li class="nav-item">
           <a class="nav-link" href="/about">About</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="/siswa">Input Data</a>
         </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/logout">Logout</a>
+        </li>
       </ul>
-      <form class="form-inline my-2 my-lg-0">
-        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-      </form>
     </div>
   </nav>
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -4,15 +4,23 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function(){
     return view('home');
-});
+})->name('home');
 
 Route::get('/about', function(){
     return view('about');
 });
-Route::get('/siswa', 'SiswaController@index')->name('siswa.view');
-Route::get('/siswa/siswatambah', 'SiswaController@create');
-Route::post('/siswa/store', 'SiswaController@store')->name('siswa.store');
-Route::post('/siswa/{id}', 'SiswaController@show')->name('siswa.show');
-Route::get('/siswa/{id}/edit', 'SiswaController@edit')->name('editData');
-Route::put('/data/{id}/update', 'SiswaController@update')->name('updateData');
-Route::get('/siswa/{id}/delete', 'SiswaController@destroy')->name('delete');
+
+Route::get('/login', 'AuthController@login')->name('login');
+Route::post('/login', 'AuthController@postlogin')->name('postLogin');
+Route::get('/logout', 'AuthController@Logout')->name('logout');
+
+Route::group(['middleware'=> 'auth'], function{}(){
+    Route::get('/siswa', 'SiswaController@index')->name('siswa.view');
+    Route::get('/siswa/siswatambah', 'SiswaController@create');
+    Route::post('/siswa/store', 'SiswaController@store')->name('siswa.store');
+    Route::post('/siswa/{id}', 'SiswaController@show')->name('siswa.show');
+    Route::get('/siswa/{id}/edit', 'SiswaController@edit')->name('editData');
+    Route::put('/data/{id}/update', 'SiswaController@update')->name('updateData');
+    Route::get('/siswa/{id}/delete', 'SiswaController@destroy')->name('delete');    
+});
+
